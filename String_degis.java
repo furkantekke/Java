56fb020b9d005710ec3d6e11a12e6d688c35b143
furkantekke <38094452+furkantekke@users.noreply.github.com>
2019-07-02 17:36:32 +0300

    Add files via upload

---
+++ String_degis.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package string_degis;
+
+import static com.sun.org.apache.xalan.internal.lib.ExsltStrings.split;
+
+/**
+ *
+ * @author furkantekke
+ */
+public class String_degis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       ekran gir = new ekran();
+       gir.setVisible(true);
+        
+     
+    }
+    
+}
